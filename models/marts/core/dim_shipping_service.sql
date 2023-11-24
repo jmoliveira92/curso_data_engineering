2d92c44c3e968787267660d4dcd9658bd0fd6ee1
Joao Oliveira <alumno25.civica@gmail.com>
2023-11-24 15:47:36 +0000

    arrange some tests

--- models/marts/core/dim_shipping_service.sql
+++ models/marts/core/dim_shipping_service.sql
@@ -1,5 +1,5 @@
 with dim_shipping_service as(
-    select distinct shipping_service from {{ ref('stg_orders_snapshot') }} order by 1 asc
+    select distinct shipping_service from {{ ref('stg_orders') }} order by 1 asc
 )
 select 
     {{dbt_utils.generate_surrogate_key(['shipping_service'])}} as shipping_service_sk,
