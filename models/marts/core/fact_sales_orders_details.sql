a6e4274d7473bc75fa738d1787c5d70cfeb15a45
Joao Oliveira <alumno25.civica@gmail.com>
2023-11-24 12:55:06 +0000

    refactor snapshots, generate all the documentation and tests, both in staging and marts folders.

--- models/marts/core/fact_sales_orders_details.sql
+++ models/marts/core/fact_sales_orders_details.sql
@@ -2,9 +2,8 @@ with stg_order_items as(
     select * from {{ ref('stg_order_items') }}
 ),
 
-stg_orders_snap as(
-select * from {{ ref('stg_orders_snapshot') }}
-where dbt_valid_to is NULL
+stg_orders as(
+select * from {{ ref('stg_orders') }}
 ),
 
 dim_promos as (
@@ -44,21 +43,20 @@ fact_sales_order_details as(
     select
     --keys
         b.order_sk,
-        a.order_id,
-        d.product_id,
+        d.product_sk,
     
     -- measures (money related)
         a.quantity_sold,
         d.unit_price_usd,
         c.discount_unit as discount_usd,
-        -- total_sales_usd is the quantity x price
+        -- total_sales_usd is the quantity_sold * unit_price_usd
         (a.quantity_sold*d.unit_price_usd) as gross_sales_usd,
-        -- extended discount amount  = (discount/100) * (quantity*unit price)
+        -- extended discount amount  = (discount_usd/100) * (quantity_sold*unit_price_usd)
         (c.discount_unit * (a.quantity_sold*d.unit_price_usd))::decimal(24,2) as discount_amount_usd,
         -- weighted shipping_cost
         (f.shipping_cost_usd*(a.quantity_sold/e.total_quantity_sold))::decimal(24,2) as shipping_cost_usd,
 
-        --net sales usd (gross_sales_ud - discount_amount_usd + shipping_cost_usd)
+        --net sales usd (gross_sales_usd - discount_amount_usd + shipping_cost_usd)
         ((a.quantity_sold*d.unit_price_usd) - (c.discount_unit * (a.quantity_sold*d.unit_price_usd)) + (f.shipping_cost_usd*(a.quantity_sold/e.total_quantity_sold)))::decimal(24,2) as net_sales_amout,
 
     -- measures (delivery/shipping related)
@@ -71,7 +69,7 @@ fact_sales_order_details as(
     left join join_orders_promos_discount c on c.order_id = a.order_id
     left join dim_products d on d.product_id = a.product_id
     left join count_order_items e on e.order_id = a.order_id
-    left join stg_orders_snap f on f.order_id = a.order_id
+    left join stg_orders f on f.order_id = a.order_id
 )
 
 select * from fact_sales_order_details
